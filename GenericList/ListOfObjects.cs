e764fc036f3f519baad20556d2429caae01452fe
Theo Holmgren <theokodar@gmail.com>
2021-01-21 11:19:17 +0100

    Began working on the generic list

---
+++ GenericList/ListOfObjects.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    class ListOfObjects<T>
+    {
+        private T[] theList;
+        private int noOfElements = 10;
+        private int nextIndex; 
+
+        public ListOfObjects()
+        {
+            theList = T[noOfElements];
+            nextIndex = 0;
+        }
+
+        public void AddObject(T obj)
+        {
+            if (nextIndex >= theList.Length)
+            {
+                Console.WriteLine("Array length exceeded");
+                return;
+            }
+            theList[nextIndex] = obj;
+            nextIndex++;
+
+        }
+    }
+}
